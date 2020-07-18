064691675aa01376d7cb5fa6ee04d8f102d725bd
Jotham <sjothamdlamini@gmail.com>
2020-07-18 05:06:10 +0200

    Integration Testing

---
+++ src/KycSystemIntegrationTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.runners.MockitoJUnitRunner;
+
+//@RunWith(JUnit4.class)
+public class KycSystemIntegrationTest {
+    @Test
+    public void testBankingK2Stystem(){
+        final String[] params = new String[]{};
+        LoginForm.main(params);
+    }
+}
